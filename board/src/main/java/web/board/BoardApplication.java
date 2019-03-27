49d338db59cd14791290f4e6643f2561f8e4d1aa
subinJeong <subin0113@estsoft.com>
2019-03-27 13:48:14 +0900

    no message

--- board/src/main/java/web/board/BoardApplication.java
+++ board/src/main/java/web/board/BoardApplication.java
@@ -1,4 +1,4 @@
-package com.board;
+package web.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- board/src/test/java/web/board/BoardApplicationTests.java
+++ board/src/test/java/web/board/BoardApplicationTests.java
@@ -1,4 +1,4 @@
-package com.board;
+package web.board;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
