8fcc1d317900d88beed5a6d83335923b557cd2e5
subinJeong <subin0113@estsoft.com>
2019-03-28 16:16:19 +0900

    no message

--- board/src/main/java/com/estsoft/web/BoardController.java
+++ board/src/main/java/com/estsoft/web/BoardController.java
@@ -2,9 +2,6 @@ package com.estsoft.web;
 
 import java.util.List;
 
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,22 +14,13 @@ import com.estsoft.service.BoardService;
 @RestController
 @RequestMapping("/board")
 public class BoardController {
-	
-	static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 
 	@Autowired
 	BoardService boardService;
 	
 	@GetMapping("/test")
 	public String test() {
-		logger.info("log test");
-		logger.debug("log debug test");
 		return "HELLO WORLD";
-	}
-	
-	@Test
-	public void test2() {
-		System.out.println("HELLO WORLD");
 	}
 	
 	@GetMapping("/writer/{id}")
