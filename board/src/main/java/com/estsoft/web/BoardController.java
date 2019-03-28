34a966f86a635b29fd058734a96d6162cab5abb4
subinJeong <subin0113@estsoft.com>
2019-03-28 13:46:47 +0900

    logback 설정 완료

--- board/src/main/java/com/estsoft/web/BoardController.java
+++ board/src/main/java/com/estsoft/web/BoardController.java
@@ -2,6 +2,9 @@ package com.estsoft.web;
 
 import java.util.List;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,13 +17,22 @@ import com.estsoft.service.BoardService;
 @RestController
 @RequestMapping("/board")
 public class BoardController {
+	
+	static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 
 	@Autowired
 	BoardService boardService;
 	
 	@GetMapping("/test")
 	public String test() {
+		logger.info("log test");
+		logger.debug("log debug test");
 		return "HELLO WORLD";
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("HELLO WORLD");
 	}
 	
 	@GetMapping("/writer/{id}")
